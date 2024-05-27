875e9a3102a2c9c8b241e73f44d68a87cc36fa39
Nartty1960 <juvinerlisw@gmail.com>
2024-05-27 16:38:34 +0300

    Create homepage.php

---
+++ .github/workflows/homepage.php
@@ -0,0 +1,18 @@
+?php
+    session_start();
+    include("connect.php");
+    ?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>homepage</title>
+</head>
+<body>
+    <div style="text-align:center; padding: 15%;">
+<p style ="font-style:50px; font-weight:bold";>
+</p>
+</div>
+
+</body>
+</html>
